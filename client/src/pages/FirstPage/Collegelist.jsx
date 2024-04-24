6a71fa90122dd99aeeaff65d72aa0f5e73209b67
ge-eth-z <geethusbabu03@gmail.com>
2024-04-24 12:27:03 +0530

    welcome

--- client/src/pages/FirstPage/Collegelist.jsx
+++ client/src/pages/FirstPage/Collegelist.jsx
@@ -2,38 +2,73 @@ import React, { useState } from 'react';
 import './Collegelist.css'; // Import your CSS file
 import backgroundImage from '././assets/welcome.jpg'; 
 import { Link } from 'react-router-dom'
+import {Carousel} from 'flowbite-react';
 
-const Collegelist = () => {
-  const [isActive, setIsActive] = useState(false);
-
-  const toggleList = () => {
-    setIsActive(!isActive);
-  };
-
+const Home = () => {
   return (
-    <div className="back" style={{backgroundImage: `url(${backgroundImage})`}}>
-      <div className={`tile-box ${isActive ? 'active' : ''}`} onClick={toggleList}>
-        <div className="content sm:mx-10">
-          {/* Content inside the tile box */}
-          <span>Click to view College List</span> 
-        </div>
-        
-        <div className="flex flex-wrap gap-8 mt-2">
+    <div className='bg-neutralSilver'>
+<div className='px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen '>
+
+<Carousel className='w-full mx-auto'>
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+          
+          
           <div>
-            <ul className={`list mb-5 ${isActive ? 'active' : ''}`}>
-              <Link to='/search' className='gap-5'><li>Jyothi Engineering College</li></Link>
-            </ul>
-            <ul className={`list mb-5 ${isActive ? 'active' : ''}`}>
-              <li>Vidya Engineering College</li>
-            </ul>
-            <ul className={`list mb-5 ${isActive ? 'active' : ''}`}>
-              <li>Govt Engineering College</li>
-            </ul>
+            <img src="https://www.pixelstalk.net/wp-content/uploads/2016/07/Beautiful-Full-HD-Images.jpg" alt="" />
+
+          </div>
+
+        {/*hero txt */}
+          <div className='md:w-1/2'>
+            <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug'>Hi I creates a portfolio 
+            <span className='text-brandPrimary leading-snug'> for everyone</span> </h1>
+            <p className='text-neutralGrey text-base mb-8'>Where to develop your portfolios</p>
+            <button className='btn-primary'>Register</button>
           </div>
         </div>
-      </div>
+
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+          
+          
+          <div>
+            <img src="https://www.pixelstalk.net/wp-content/uploads/2016/07/Beautiful-Full-HD-Images.jpg" alt="" />
+
+          </div>
+
+        {/*hero txt */}
+          <div className='md:w-1/2'>
+            <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug'>Learn And 
+            <span className='text-brandPrimary leading-snug'> Grow</span> </h1>
+            <p className='text-neutralGrey text-base mb-8'>Where to develop your portfolios</p>
+            <button className='btn-primary'>Register</button>
+          </div>
+        </div>
+
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+          
+          
+          <div>
+            <img src="https://www.pixelstalk.net/wp-content/uploads/2016/07/Beautiful-Full-HD-Images.jpg" alt="" />
+
+          </div>
+
+        {/*hero txt */}
+          <div className='md:w-1/2'>
+            <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug'>Make your own 
+            <span className='text-brandPrimary leading-snug'> Portfolio</span> </h1>
+            <p className='text-neutralGrey text-base mb-8'>Where to develop your portfolios</p>
+            <button className='btn-primary'>Register</button>
+          </div>
+        </div>
+
+        
+
+      </Carousel>
+
+</div>
+
     </div>
-  );
+  )
 }
 
-export default Collegelist;
+export default Home
