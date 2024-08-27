4dc602f81ec3d4bffdf62bf0f8d6aafafab964b2
ALAN JOSE <130158432+Alanj7788@users.noreply.github.com>
2024-08-28 01:01:52 +0530

    host

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use('/api/idea', ideaRoute);
 
 app.use(express.static('users'));
 
+app.get('/', (req, res) => {
+    res.send('Server is up and running!');
+  });
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
