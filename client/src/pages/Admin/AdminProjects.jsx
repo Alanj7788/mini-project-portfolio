62b8ef846bda52ba50a87c7ff61b00a0fe7ba1fe
ALAN JOSE <130158432+Alanj7788@users.noreply.github.com>
2024-09-06 14:10:00 +0530

    Update AdminProjects.jsx

--- client/src/pages/Admin/AdminProjects.jsx
+++ client/src/pages/Admin/AdminProjects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+const apiUrl = process.env.REACT_APP_API_URL;
 import { useSelector, useDispatch } from 'react-redux'
 import {Form, Modal, message} from "antd";
 import { HideLoading, ReloadData, ShowLoading } from '../../redux/rootSlice';
