4685539926f2f70ce9ab8e63f6d9b0802aa63969
ALAN JOSE <130158432+Alanj7788@users.noreply.github.com>
2024-09-06 14:13:32 +0530

    Update AdminExperience.jsx

--- client/src/pages/Admin/AdminExperience.jsx
+++ client/src/pages/Admin/AdminExperience.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-const apiUrl = process.env.REACT_APP_API_URL;
+
 import { useSelector, useDispatch } from 'react-redux'
 import {Form, Modal, message} from "antd";
 import { HideLoading, ReloadData, ShowLoading } from '../../redux/rootSlice';
@@ -7,6 +7,11 @@ import axios from 'axios';
 
 
 function AdminExperience() {
+
+
+
+
+const apiUrl = process.env.REACT_APP_API_URL;
     const dispatch=useDispatch();
     const {portfolioData}= useSelector((state)=>state.root);
     const {experiences} = portfolioData; 
